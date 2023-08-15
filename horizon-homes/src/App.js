0be6e7c71b2038df76f2a7c4adcbfc254ee3dc65
thatkhay <johnkingsleyegeonu@gmail.com>
2023-08-15 03:11:29 +0100

    created routes to navigate around

--- horizon-homes/src/App.js
+++ horizon-homes/src/App.js
@@ -1,13 +1,27 @@
-
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
 
 
 function App() {
   return (
-    <div className="App">
+    <>
 
-     <h1>my app</h1>
-         
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signin" element={<Signup />} />
+          <Route path="/signin" element={<ForgotPassword />} />
+          </Routes>
+          </Router>
+    </>
   );
 }
 
---
+++ horizon-homes/src/pages/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ForgotePassword = () => {
+  return (
+    <div>Forgote Password</div>
+  )
+}
+
+export default ForgotePassword
