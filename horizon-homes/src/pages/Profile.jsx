ee43dd3a24ffd0564b55cc94d58b7176d159fe4a
thatkhay <johnkingsleyegeonu@gmail.com>
2023-08-18 13:54:26 +0100

    .

--- horizon-homes/src/pages/Profile.jsx
+++ horizon-homes/src/pages/Profile.jsx
@@ -10,7 +10,7 @@ console.log(auth.currentUser);
 setUser(auth.currentUser)
   }, [])
   return (
-    user ? <h1>{user.displayName}</h1> :  'Not logged in'
+    user ? <h1>{user.displayName}</h1> :  'Not Logged in'
   )
 }
 
