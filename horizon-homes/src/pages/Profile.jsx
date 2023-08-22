f5e352fb9d9dccae5b3a3b453758fcc9b92ecd59
thatkhay <johnkingsleyegeonu@gmail.com>
2023-08-22 22:20:50 +0100

    created a logic to update name in fire store

--- horizon-homes/src/pages/Profile.jsx
+++ horizon-homes/src/pages/Profile.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
-import { getAuth } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom'
+import { getAuth , updateProfile} from 'firebase/auth'
+import { useNavigate, Link } from 'react-router-dom'
+import { updateDoc , doc} from 'firebase/firestore' 
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
 
 const Profile = () => {
   const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -14,6 +18,32 @@ const navigate = useNavigate
     auth.signOut()
     navigate('/')
   }
+
+  const onSubmit = async () => {
+    try{
+if (auth.currentUser.displayName !== name) {
+  // update display name in fireBase
+  await updateProfile(auth.currentUser, {
+    displayName: name
+  })
+
+  // update in firestore
+  const userRef = doc(db, 'users', auth.currentUser.uid)
+  await updateDoc(userRef, {
+    name
+  })
+}
+    }catch (error){
+toast.error('Could not update user Profile')
+    }
+  }
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+    ...prev,
+    [e.target.id]: e.target.value,
+    }))
+  }
   
   return (
 <div className='profile'>
@@ -21,6 +51,23 @@ const navigate = useNavigate
   <p className="pageHeader">My Profile</p>
   <button type='button' className='logOut' onClick={onLogout}>logout</button>
 </header>
+<main>
+  <div className="profileDetailsHeader">
+    <p className="profileDetailsText">Personal Deatails</p>
+    <p className="changePersonalDetails" onClick={() => {
+      changeDetails && onSubmit()
+      setChangeDetails((prev) => !prev)
+    }}>
+      {changeDetails ? 'done' : 'change'}
+    </p>
+  </div>
+  <div className="profileCard">
+    <form action="">
+      <input type="text"  id='name' className={!changeDetails ? 'profileName' : 'profileNameActive'} disabled={!changeDetails} value={name} onChange={onChange}/>
+      <input type="text"  id='email' className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} disabled={!changeDetails} value={email} onChange={onChange}/>
+    </form>
+  </div>
+</main>
 </div>
   )
 }
