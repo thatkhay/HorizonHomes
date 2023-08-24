5ae8f0452ed82ac5af8514c9e74c91e1bb42bc95
thatkhay <johnkingsleyegeonu@gmail.com>
2023-08-24 22:23:29 +0100

    added explore page

--- horizon-homes/src/pages/Explore.jsx
+++ horizon-homes/src/pages/Explore.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import rentalImgCategory from '../assets/jpg/rentCategoryImage.jpg'
+import sellImgCategory from '../assets/jpg/sellCategoryImage.jpg'
 
 const Explore = () => {
   return (
-    <div>Explore</div>
+    <div className='explore'>
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+      <main>
+        {/* slider */}
+        <p className="exploreCategoryHeading">Categories</p>
+        
+        <div className="exploreCategories">
+          <Link to='/category/rent'>
+         <img src={rentalImgCategory} alt="rent"  className='exploreCategoryImg'/>
+         <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+
+          <Link to='/category/sell'>
+         <img src={sellImgCategory} alt="sell"  className='exploreCategoryImg'/>
+         <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
+
+      </main>
+      
+    </div>
   )
 }
 
