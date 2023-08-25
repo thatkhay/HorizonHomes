e5223dbee06c583aa969aa159668a65296b0d184
thatkhay <johnkingsleyegeonu@gmail.com>
2023-08-25 12:46:35 +0100

    replaced the h3 with a listingItem component

--- horizon-homes/src/pages/Category.jsx
+++ horizon-homes/src/pages/Category.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import Spinner from '../components/Spinner';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 const Category = () => {
     const [listings, setListinggs] = useState(null);
@@ -58,7 +59,7 @@ try {
     <main>
         <ul className="categoryListings">
             {listings.map((listing) => (
-                <h3 key={listing.id}>{listing.data.name}</h3>
+                <ListingItem listing={listing.data} id={listing.id} key={listing.id}/>
             ))}
         </ul>
     </main>
